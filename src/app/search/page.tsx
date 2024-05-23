fa4a8e258a69ecdb53c02ce56e922153daa71e36
hjthebunny <monicahyunji@gmail.com>
2024-05-23 18:10:07 +0900

    feat: fetch data

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,7 +2,8 @@ import SearchBar from "../ui/searchBar";
 import Card from "../ui/card";
 import Logo from "../ui/logo";
 
-const Search = () => {
+const Search = async () => {
+  const data = await searchPlaylists("lucy");
   const arr = [0, 0, 0, 0, 0, 0];
   const list = arr.map((el, index) => {
     return <Card key={index} />;
@@ -25,3 +26,14 @@ const Search = () => {
 };
 
 export default Search;
+
+const searchPlaylists = async (query: string) => {
+  const res = await fetch(
+    `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=12&regionCode=kr&q=${query}&key=${process.env.NEXT_PUBLIC_YOUTUBE_KEY}`
+  );
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+  const data = await res.json();
+  return data;
+};
